68a52c21d2c0a7f32e1b6b917d796f0b78a2912b
rodrigo-lange <rodrigolange@gmail.com>
2021-07-10 23:57:15 -0300

    Create script_vm_inicial.sh

---
+++ script_vm_inicial.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#vai para pasta home
+cd ~
+
+#instala os pacotes essenciais
+apt install python3-pip python-setuptools build-essential python-dev -y
+
+#instala o PIP2
+cd ~
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+python get-pip.py
+
+#instala o volatility
+cd ~
+git clone https://github.com/volatilityfoundation/volatility.git 
+cd volatility 
+chmod +x vol.py
+python setup.py build 
+python setup.py build install
+
+#instala as dependencias do volatility
+pip3 install --upgrade setuptools
+pip2 install --upgrade setuptools
+pip2 install pycrypto
+pip2 install distorm3==3.4.4 
+
+#instala plaso e log2timeline
+cd ~
+git clone https://github.com/log2timeline/plaso.git
+cd plaso
+python3 setup.py build
+python3 setup.py build install
+pip3 install -r requirements.txt
+pip3 install fakeredis

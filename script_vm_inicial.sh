4852a6def8fa6ce5a2f69a1215467ca751e9a544
rodrigo-lange <rodrigolange@gmail.com>
2022-01-08 16:45:42 -0300

    Update script_vm_inicial.sh

--- script_vm_inicial.sh
+++ script_vm_inicial.sh
@@ -44,7 +44,7 @@ sudo wget -N https://github.com/rodrigo-lange/linux-forense/raw/main/profile.zip
 # instala plaso e log2timeline (por incompatibilidade entre as bibliotecas Yara do Volatility e do Plaso, optou-se pela instalação via docker)
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get install docker.io
+sudo apt-get install -y docker.io
 sudo systemctl enable docker --now
 docker pull log2timeline/plaso
 
